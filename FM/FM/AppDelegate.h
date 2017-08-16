cd31c8b163b278bd33955a98e8b54d8a5a627e96
home <home@qpdeMacBook-Air.local>
2017-08-16 13:59:43 +0800

    初始化

---
+++ FM/FM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FM
+//
+//  Created by home on 2017/8/16.
+//  Copyright © 2017年 qingpengwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
